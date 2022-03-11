398bae18f5a4ad3bf85b6e476384b5f1af16b72d
Jose A González <jagascripts@gmail.com>
2022-03-12 00:07:39 +0100

    Algunas Clases de Python y Java

---
+++ Java/Car.java
@@ -0,0 +1,8 @@
+package CursoPOOUber.Java;
+
+public class Car {
+    Integer id;
+    String license;
+    String driver;
+    Integer passenger;    
+}
---
+++ Python/main.py
@@ -0,0 +1,7 @@
+from json.tool import main
+from socket import if_nameindex
+
+
+if __name__ == "__main__":
+    print("Hola Mundo")
+    
