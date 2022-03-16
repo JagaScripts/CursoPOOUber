3ff46bf1c3c2b831056f16de250e136b5f3c8eb6
JoseAGonzalezA <jose.a.gonzalez.alcantara@gmail.com>
2022-03-17 00:22:15 +0100

    probado php

--- PHP/index.php
+++ PHP/index.php
@@ -2,6 +2,12 @@
 
 require_once('Car.php');
 require_once('Account.php');
+require_once('UberX.php');
+
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "ADN456"), "Chevrolet", "Spark");
+$uberX->PrintDataCar();
 
 $car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
 $car->printDataCar();
+
+?>
