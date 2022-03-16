6c39636625359eb4c380a8184059a80b9b1b53a2
JoseAGonzalezA <jose.a.gonzalez.alcantara@gmail.com>
2022-03-16 23:16:52 +0100

    Constructores en python y php

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,17 @@
 from car import Car
+from account import Account
 from json.tool import main
 from socket import if_nameindex
 
 
 if __name__ == "__main__":
     print("Hola Mundo")
+
+    car = Car("AMQ123", Account("Andres Herrera","ANDA876"))
+    print(vars(car))
+    print(vars(car.driver))
+
+    '''Antes de añadir __init__
     car = Car()
     car.license = "AMQ123";
     car.driver = "Andres Herrera";
@@ -16,3 +23,4 @@ if __name__ == "__main__":
     car1.driver = "Andrea Herrera";
     car1.passenger = 3;
     print(vars(car1))
+    '''
